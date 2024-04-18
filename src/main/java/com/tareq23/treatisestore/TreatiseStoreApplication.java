15ef96109f77e60835f45177f130d5d213f41b24
Md Tarequl Islam <mdtarequlislam634@gmail.com>
2024-04-19 02:58:32 +0600

    init

---
+++ src/main/java/com/tareq23/treatisestore/TreatiseStoreApplication.java
@@ -0,0 +1,13 @@
+package com.tareq23.treatisestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreatiseStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreatiseStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tareq23/treatisestore/TreatiseStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tareq23.treatisestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreatiseStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
